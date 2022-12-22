61ad1127ec3703e6a63442defa8c49965239f044
Rookin21 <crazynick21@gmail.com>
2022-12-23 00:26:32 +0300

    Выполнена первая часть задания. Создан класс, добавлены переменные и реализован вывод данных на консоль

--- IntroOOP/BankAccount.cs
+++ IntroOOP/BankAccount.cs
@@ -1,17 +1,26 @@
+using System.Xml.Linq;
+
 public class BankAccount
 {
     private double _Num;
     private decimal _Balance;
+    private Card _TypeCard;
 
-    public enum _TypeCard
+    public enum Card
     {
-        _BankCard,
-        _CreditCard,
-        _DebitCard
+        BankCard,
+        CreditCard,
+        DebitCard
     };
 
-    public BankAccount() { }
+    public double Num { get => _Num; set => _Num = value; }
+    public decimal Balance { get => _Balance; set => _Balance = value; }
+    public Card TypeCard { get => _TypeCard; set => _TypeCard = value; }
 
-    public double Num { get; set; }
-    public decimal Balance { get; set; }
+    public BankAccount(double num, decimal balance, Card typeCard)
+    {
+        _Num = num;
+        _Balance = balance;
+        _TypeCard = typeCard;
+    }
 }
--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("123");
+﻿using static BankAccount;
+
+Console.WriteLine("ДЗ. Урок 2. Две роли класса в ООП");
+
+BankAccount bankAccount = new BankAccount(666, 999.999M, BankAccount.Card.DebitCard);
+
+Console.WriteLine("\n№ счета: {0} \nБаланс: {1} \nТип банковского счета: {2}", bankAccount.Num, bankAccount.Balance, bankAccount.TypeCard);
+
+Console.ReadKey(true);
