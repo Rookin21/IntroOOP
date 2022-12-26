e4d1dd2a0a23dc017b99f64d6b48f543e5f38c0b
Rookin_work <crazynick21@gmail.com>
2022-12-26 07:59:10 +0300

    Доавлены комментарии к первому заданию.

--- IntroOOP/BankAccount.cs
+++ IntroOOP/BankAccount.cs
@@ -2,21 +2,33 @@ using System.Xml.Linq;
 
 public class BankAccount
 {
-    private double _Num;
-    private decimal _Balance;
-    private Card _TypeCard;
+    /// <summary>
+    /// Приватнные полы
+    /// </summary>
+    private double _Num; // Номер счета    
+    private decimal _Balance; // Баланс
+    private Card _TypeCard; // Тип банковского счета
 
-    public enum Card
+    public enum Card // Перечислимое - тип банковского счета
     {
         BankCard,
         CreditCard,
         DebitCard
     };
 
+    /// <summary>
+    /// Чтение и запись приватных полей
+    /// </summary>
     public double Num { get => _Num; set => _Num = value; }
     public decimal Balance { get => _Balance; set => _Balance = value; }
     public Card TypeCard { get => _TypeCard; set => _TypeCard = value; }
 
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="num">Номер счета</param>
+    /// <param name="balance">Баланс</param>
+    /// <param name="typeCard">Тип банковского счета</param>
     public BankAccount(double num, decimal balance, Card typeCard)
     {
         _Num = num;
--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine("ДЗ. Урок 2. Две роли класса в ООП");
 
-BankAccount bankAccount = new BankAccount(666, 999.999M, BankAccount.Card.DebitCard);
+BankAccount bankAccount = new BankAccount(666, 999.999M, BankAccount.Card.DebitCard); // Заполнение конструктора
 
 Console.WriteLine("\n№ счета: {0} \nБаланс: {1} \nТип банковского счета: {2}", bankAccount.Num, bankAccount.Balance, bankAccount.TypeCard);
 
